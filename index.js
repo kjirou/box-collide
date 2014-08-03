028e3628193de88e74c32776b7ad0cb1f532d34b
James Halliday <mail@substack.net>
2014-08-03 13:55:06 +1200

    inside fix

--- index.js
+++ index.js
@@ -2,15 +2,23 @@ module.exports = function (ra, rb) {
     var a = norm(ra), b = norm(rb);
     var inx = isect(a.left, b.left, b.right)
         || isect(a.right, b.left, b.right)
+        || inside(a.left, a.right, b.left, b.right)
+        || inside(b.left, b.right, a.left, a.right)
     ;
     var iny = isect(a.top, b.top, b.bottom)
         || isect(a.bottom, b.top, b.bottom)
+        || inside(a.top, a.bottom, b.top, b.bottom)
+        || inside(b.top, b.bottom, a.top, a.bottom)
     ;
     return inx && iny;
 };
 
 function isect (x, lower, upper) {
     return x >= lower && x <= upper;
+}
+
+function inside (a0, a1, b0, b1) {
+    return a0 >= b0 && a1 <= b1;
 }
 
 function norm (q) {
